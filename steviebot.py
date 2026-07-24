7a8fdb75775ad92bf6b7289fbd981527b32d0373
StevieG892003 <stephengreenhalgh13@gmail.com>
2026-07-25 00:28:04 +0100

    Update steviebot.py

--- steviebot.py
+++ steviebot.py
@@ -1,23 +1,18 @@
 import discord
 from discord.ext import commands
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
-@bot.event
-async def on_ready():
-    print("Bot is online and ready!")
 
 @bot.event
 async def on_ready():
-    print("Bot is online and ready!")
+    print(f"Bot is online as {bot.user}")
 
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong!")
 
-import os
 bot.run(os.getenv("DISCORD_TOKEN"))
-
-
