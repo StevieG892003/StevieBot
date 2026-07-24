0708632b070b396802e4b09698ebea72336af8d8
StevieG892003 <stephengreenhalgh13@gmail.com>
2026-07-25 00:19:41 +0100

    Remove duplicate on_ready event handler

--- steviebot.py
+++ steviebot.py
@@ -5,6 +5,9 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
+@bot.event
+async def on_ready():
+    print("Bot is online and ready!")
 
 @bot.event
 async def on_ready():
