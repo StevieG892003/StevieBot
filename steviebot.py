89f3dcee41b42db7668347dbc6d09975d957ed4d
StevieG892003 <stephengreenhalgh13@gmail.com>
2026-07-25 01:50:34 +0100

    Fix indentation and update string quotes in steviebot.py

--- steviebot.py
+++ steviebot.py
@@ -5,14 +5,14 @@ import os
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-  print(f"Bot is online as {bot.user}")
+    print(f"Bot is online as {bot.user}")
 
 @bot.command()
 async def ping(ctx):
-  await ctx.send("Pong!")
+    await ctx.send("pong")
 
 bot.run(os.getenv("DISCORD_TOKEN"))
